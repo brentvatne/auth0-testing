9f57c23359672976295c272f5570ac1fae76cf31
Brent Vatne <brentvatne@gmail.com>
2020-12-12 17:36:36 -0800

    example

--- App.tsx
+++ App.tsx
@@ -1,12 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as AuthSession from "expo-auth-session";
+import jwtDecode from "jwt-decode";
+import * as React from "react";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
+
+const auth0ClientId = "TrfNc27G6IB0h3Xjq5ymIcpTF6GsKQ74";
+const authorizationEndpoint = "https://brentvatne.auth0.com/authorize";
+
+const useProxy = false;
+const redirectUri = AuthSession.makeRedirectUri({ useProxy });
+
+console.log(redirectUri);
 
 export default function App() {
+  const [name, setName] = React.useState(null);
+
+  const [request, result, promptAsync] = AuthSession.useAuthRequest(
+    {
+      redirectUri,
+      clientId: auth0ClientId,
+      // id_token will return a JWT token
+      responseType: "id_token",
+      // retrieve the user's profile
+      scopes: ["openid", "profile"],
+      extraParams: {
+        // ideally, this will be a random value
+        nonce: "nonce",
+      },
+    },
+    { authorizationEndpoint }
+  );
+
+  // Retrieve the redirect URL, add this to the callback URL list
+  // of your Auth0 application.
+  console.log(`Redirect URL: ${redirectUri}`);
+
+  React.useEffect(() => {
+    if (result) {
+      // @ts-ignore
+      if (result.error) {
+        Alert.alert(
+          "Authentication error",
+          // @ts-ignore
+          result.params.error_description || "something went wrong"
+        );
+        return;
+      }
+      if (result.type === "success") {
+        // Retrieve the JWT token and decode it
+        const jwtToken = result.params.id_token;
+        const decoded = jwtDecode(jwtToken);
+
+        // @ts-ignore
+        const { name } = decoded;
+        setName(name);
+      }
+    }
+  }, [result]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {name ? (
+        <Text style={styles.title}>You are logged in, {name}!</Text>
+      ) : (
+        <Button
+          disabled={!request}
+          title="Log in with Auth0"
+          onPress={() => promptAsync({ useProxy })}
+        />
+      )}
     </View>
   );
 }
@@ -14,8 +75,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    textAlign: "center",
+    marginTop: 40,
   },
 });
